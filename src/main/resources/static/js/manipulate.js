fbb8e4339208427fc44170a496f0c84a439b62c6
Shuplat Orest <shuplatorst@gmail.com>
2020-11-17 17:35:48 +0200

    Adding web to application

---
+++ src/main/resources/static/js/manipulate.js
@@ -0,0 +1,15 @@
+/*
+const removeBtn = document.getElementById('remove__button');
+
+
+function deleteAirplane(id){
+    const formData = new FormData();
+    let delete_endpoint = 'http://localhost:8080/airlines/' + id;
+    document.getElementById("item"+id).remove();
+    return fetch(delete_endpoint, {
+        method: 'DELETE',
+        body: formData
+    });
+}*/
+
+
